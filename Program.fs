476c5b42333eaae29c588ec51a044614abb2b451
Han Tran <hantr060902@gmail.com>
2025-05-06 00:11:40 -0500

    Adjusting Program.fs to fit the new changes

--- Program.fs
+++ Program.fs
@@ -2,6 +2,8 @@ open Giraffe
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Routes
 
 open Parse
 open Fun
@@ -9,22 +11,39 @@ open Fun
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    
+    // Add Giraffe to the service collection
     builder.Services.AddGiraffe() |> ignore
+    
+    // Configure CORS to allow API requests
+    builder.Services.AddCors(fun options ->
+        options.AddPolicy("AllowAll", fun builder ->
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                |> ignore
+        )
+    ) |> ignore
 
     let app = builder.Build()
+    
+    // Configure middleware
     app.UseStaticFiles() |> ignore
+    app.UseCors("AllowAll") |> ignore
 
-
-    // app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    // Configure routing
     let webApp =
         choose [
             route "/" >=> htmlFile "./wwwroot/index.html"
-            // your other routes
+            UserInputRoutes.routes  // This includes our convert-ml route
+            // Fallback for other routes
+            setStatusCode 404 >=> text "Not Found"
         ]
     app.UseGiraffe(webApp)
 
+    // Output some examples to verify our parsing works
     printfn "%A" Parse.e1
-
     printfn "%A" Parse.e2
 
     eval Parse.e1 [] |> printfn "%A"    
@@ -36,4 +55,3 @@ let main args =
     app.Run()
 
     0 // Exit code
-
